1e804ac2514ff68a59c2940e15c02b769fd66109
Rick Ertl <ertl.rick@gmail.com>
2022-11-13 12:10:46 -0600

    first stab at models

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import ChoreEvent, Family, Dog
+
+# admin.site.register(ChoreEvent)
+admin.site.register(Family)
+admin.site.register(Dog)
+
+@admin.register(ChoreEvent)
+class ChoreEventAdmin(admin.ModelAdmin):
+    """Administration object for ChoreEvent models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - filters that will be displayed in sidebar (list_filter)
+     - grouping of fields into sections (fieldsets)
+    """
+    list_display = ('type', 'dog', 'care_giver', 'timestamp')
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User  # Required to assign User care_giver
 
-# Create your models here.
+
+# Chore Event
+class ChoreEvent(models.Model):
+    """Model representing a chore event."""
+
+    CHORE_TYPES = (
+        ('f', 'Feed'),
+        ('w', 'Walk'),
+        ('p1', 'Pee'),
+        ('p2', 'Poop'),
+    )
+
+    type = models.CharField(max_length=2, choices=CHORE_TYPES)
+
+    dog = models.ForeignKey('Dog', on_delete=models.SET_NULL, null=True)
+
+    care_giver = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) 
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.type
+
+
+# Family
+class Family(models.Model):
+    """Model representing a family."""
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+
+# Dog 
+class Dog(models.Model):
+    """Model representing a dog."""
+    name = models.CharField(max_length=200)
+
+    family = models.ForeignKey(Family, on_delete=models.SET_NULL, null=True) 
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
